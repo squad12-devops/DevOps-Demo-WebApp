d07cf2b316028a15150123c43ab3c75cae9ee9ba
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-02 19:58:11 -0500

    Updated to Deepika's AWS test server

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://172.31.46.18:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://100.26.56.194/postgres";
 					String username="postgres";
 					String password="password";
 					
