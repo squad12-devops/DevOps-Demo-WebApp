ec4d927df1d64f81acd2a9e32965f17c72b2347a
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 11:39:03 -0500

    Updated to Deepika's AWS Test server

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://100.25.194.166/postgres";
+			String jdbcUrl="jdbc:postgresql://100.25.194.166:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
