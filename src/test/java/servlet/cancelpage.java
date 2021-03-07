6ced79301dc76baed486e808a0d576738d4880d6
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 00:17:57 -0500

    ip

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://172.31.61.73/postgres";
+					String jdbcUrl="jdbc:postgresql://172.31.41.186/postgres";
 					String username="postgres";
 					String password="password";
 					
