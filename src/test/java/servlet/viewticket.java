e9473338792145352d128589e1b6ca8d9c1a774b
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 01:13:18 -0500

    vino ip

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://172.31.47.224:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.160.90.211:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
