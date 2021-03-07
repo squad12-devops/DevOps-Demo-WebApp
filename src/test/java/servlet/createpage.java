219de942ad2dda908b13d42dcb3ea0d4b862c9a4
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 01:12:55 -0500

    vino ip

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://172.31.47.224/postgres";
+			String jdbcUrl="jdbc:postgresql://54.160.90.211/postgres";
 			String username="postgres";
 			String password="password";
 		
