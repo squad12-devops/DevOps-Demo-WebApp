01308e8c292654221b7bd8a9c6c9e32e0dd42dcf
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 00:01:32 -0500

    qa private ip

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://54.144.155.185/postgres";
+			String jdbcUrl="jdbc:postgresql://172.31.61.73/postgres";
 			String username="postgres";
 			String password="password";
 		
