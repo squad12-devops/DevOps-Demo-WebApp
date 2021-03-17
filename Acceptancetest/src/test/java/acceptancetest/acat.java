2930d7c093a09d868ae33f801b9137ff17ba0be9
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-17 18:58:15 -0400

    Updated to Deepika's PROD server

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -12,8 +12,8 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://172.31.4.99:8080/ProdWebapp";
-	public String URL1 = "http://172.31.4.99:8080/ProdWebapp/avncreatepage.jsp";
+	public String URL = "http://54.236.53.31:8080/ProdWebapp";
+	public String URL1 = "http://54.236.53.31:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	
